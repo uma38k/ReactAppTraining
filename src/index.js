2474b0843557621fd515fa6047a93ddd3c8c6ad5
kumari <uma.a.kumari@capgemini.com>
2025-07-02 16:01:30 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// first component
+function Clock() {
+  return(<>    
+      <h2>Current time is- </h2>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </>)
+}
+
+// second component
+function Main(props) {
+  //console.log(props)
+  return(<div>
+      <h1>Hello World!! - {props.msg}</h1>
+      <h2>Welcome to React!!!</h2>
+      {props.children}
+      <h2>Contact Us</h2>
+    </div>)
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root.render(<Main msg="Capgemini" fn="Hello" ln="World!!">
+//   <Clock/>
+// </Main>);
+
+setInterval(()=> {
+  root.render(<Main msg="Capgemini" fn="Hello" ln="World!!">
+    <Clock/>
+  </Main>);
+}, 1000)
+
+//root.render(React.createElement(Main, { msg: "Capgemini", fn: "Hello", ln: "World!" }, "children in createElement"));
+//root.render(Main('Capgemini'));
