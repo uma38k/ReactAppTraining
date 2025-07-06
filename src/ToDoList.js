8d25b4455a76e834ab8c5ab07d2f5ca0b564232e
kumari <uma.a.kumari@capgemini.com>
2025-07-07 01:02:23 +0530

    to do app

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -4,44 +4,55 @@ import './css/todolist.css';
 function ToDoList() {
     const [task, setTask] = useState('');
     const [lists, setLists] = useState([]);
+    const [count, setCount] = useState(0);
+
     const addItem = ()=> {
         if (task.trim() === '') {
             alert("Input field is empty");
             return;
         }
-        setLists([...lists, {id:crypto.randomUUID(), name:task.trim(), completed:false}]);
+        setLists([...lists, {id: crypto.randomUUID(), name: task.trim(), completed: false}]);
         setTask('');
+        setCount(lists.length+1);
     }
+
     const toggleComplete = (id)=> {
-        setLists(
-            lists.map((task) => 
-                task.id === id ? { ...lists, completed:!task.completed} : task
+        setLists(lists =>
+            lists.map(task => 
+                task.id === id 
+                ? {...task, completed: !task.completed} 
+                : task
             )
         );
     };
+
     const removeItem = (id)=> {
         setLists(lists.filter((task) => task.id !== id));
+        setCount(lists.length-1);
     };
+
     return (
-        <div className='todo-list'>  
-            <h3>To Do List</h3>
+        <div className='todo-app'>  
+            <h3 className='heading'>To Do List</h3>
             
-            <input 
-                type="text" 
-                placeholder='Enter a text'
-                value={task} 
-                onChange={(e) => setTask(e.target.value)} />
-            <button onClick={addItem}>Add Item</button>
+            <div className='input-action-container'>
+                <input 
+                    id="new-task"
+                    type="text" 
+                    placeholder='Enter new task here'
+                    value={task} 
+                    onChange={(e) => setTask(e.target.value)} />
+                <button className="add-task" onClick={addItem}>+</button>
+            </div>
 
             <hr/>
             
-            <ul>
+            <ul className='todo-list'>
                 {lists.map((task) => (
                     <li key={task.id} className={task.completed ? 'completed' : ''} >
                         <span className='toggle-complete' onClick={()=> toggleComplete(task.id)}></span>
                         <span>{task.name}</span>
-                        <button onClick={(e)=> {
-                            e.stopPropagation();
+                        <button onClick={()=> {
                             removeItem(task.id)
                         }}>
                             x
@@ -49,6 +60,8 @@ function ToDoList() {
                     </li>
                 ))}
             </ul>
+
+            <div className='task-count'>Task Count {count}</div>
         </div>
     );
 }
