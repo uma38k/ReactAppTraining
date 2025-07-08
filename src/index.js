39bb3fcf16171502a2aa9996a1aa8777c5c477c8
kumari <uma.a.kumari@capgemini.com>
2025-07-08 22:50:39 +0530

    React Route - loadable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loadable from 'react-loadable';
 import Nav from './pages/Nav';
 import CountCalculation from './pages/UseEffect';
 import Component1 from './pages/UseContext';
-import ToDoList from './components/ToDoList';
+//import ToDoList from './components/ToDoList';
 import Counter from './pages/Counter';
 import Clock from './pages/Clock';
 import Main from './pages/Main';
 import ListKey from './pages/ListKey';
-import WeatherApp from './components/WeatherApp';
+//import WeatherApp from './components/WeatherApp';
+
+function Loading({error}) {
+ if (error) {
+  return 'Oh No!!'
+ } else {
+  return <h3>Loading...</h3>
+ }
+}
+
+const ToDoList = Loadable ({
+  loader: ()=> import('./components/ToDoList'),
+  loading: Loading
+});
+
+const WeatherApp = Loadable ({
+  loader: ()=> import('./components/WeatherApp'),
+  loading: Loading
+});
 
 export default function App() {
     return (
